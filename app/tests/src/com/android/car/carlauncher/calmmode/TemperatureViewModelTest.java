ad581835a1c5dec148548f3d3c548a3396b87e3b
Tim Froehlich <froeht@google.com>
2025-02-24 08:10:50 -0800

    Move InstantTaskExecutorRule to test package and eliminate uses of the version from car-apps-common.
    `car-apps-common` improperly included the rule in it's main (non-test) code.  It's being refactored out to `car-testing-common`. Launcher already had a copy of the file, but in the wrong directory, so that file is being moved to the app/tests directory and the tests are having their includes changed to use that version.
    
    Bug: 389088738
    Test: mma CarLauncherTests
    Flag: EXEMPT refactor
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:e2b58edfe7c67f5f9ad3465e43b04eeebd593680)
    Merged-In: I597a546dcbe3ab872028648ff961b66b24ade73b
    Change-Id: I597a546dcbe3ab872028648ff961b66b24ade73b

--- app/tests/src/com/android/car/carlauncher/calmmode/TemperatureViewModelTest.java
+++ app/tests/src/com/android/car/carlauncher/calmmode/TemperatureViewModelTest.java
@@ -42,7 +42,7 @@ import androidx.annotation.NonNull;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.android.car.apps.common.testutils.InstantTaskExecutorRule;
+import com.android.car.carlauncher.InstantTaskExecutorRule;
 
 import org.junit.Before;
 import org.junit.Rule;
